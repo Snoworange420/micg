4c6716d337fdbb067af58f15f3114d32a66d8c4b
n401571 <n401571@gymnasiumnovum.nl>
2023-02-22 13:03:42 +0100

    wtf palyer clazz forgot?!?!?

---
+++ player.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+def saveConfig(username):
+
+    filename = "config.json"
+
+    if filename not in os.listdir('.'):
+        with open(filename, 'w') as file:
+            json.dump({}, file)
+            return
+
+
+    with open(filename, 'w') as file:
+        data = {
+            "config": {
+                "username": username,
+            }
+        }
+
+        json.dump(data, file)
+
+def _update_user_name(name: str):
+    import main
+    main.username = name
+    pass
+        
+def loadConfig():
+
+    filename = "config.json"
+
+    if filename not in os.listdir('.'):
+        with open(filename, 'w') as file:
+            json.dump({}, file)
+            return
+
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+        _update_user_name(data["config"]["username"])
